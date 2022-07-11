d11b85b21409bd97ddf090db067d1fc5d091c5ab
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2022-07-11 10:41:39 +0000

    chore: add numpy

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from bytewax import Dataflow, inputs, spawn_cluster, AdvanceTo, Emit
+from bytewax import Dataflow, inputs, parse, run_cluster, AdvanceTo, Emit, spawn_cluster
 from websocket import create_connection
 import json
 
-
-def ws_input():
+def input_builder(worker_index, worker_count):
     ws = create_connection("wss://ftx.com/ws")
     ws.send(
         json.dumps(
@@ -19,11 +18,6 @@ def ws_input():
         yield Emit(ws.recv())
         epoch += 1
         yield AdvanceTo(epoch)
-
-def input_builder(worker_index, worker_count):
-    # prods_per_worker = int(len(PRODUCT_IDS)/worker_count)
-    # product_ids = PRODUCT_IDS[int(worker_index*prods_per_worker):int(worker_index*prods_per_worker+prods_per_worker)]
-    return ws_input()
 
 
 def output_builder(worker_index, worker_count):
